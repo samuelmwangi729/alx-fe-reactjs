b2808bc35cd49e60cf84de9a54eefe01a8f942ac
samuelmwangi729 <samuelmwangi729@gmail.com>
2025-08-31 12:50:25 +0300

    completed the assignment

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
+      {/* New app.jsx  */}
+      {/* <WelcomeMessage /> */}
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
       <Footer />
