9bb4e514b81df93699c58fae56c2bda0f8221bfb
samuelmwangi729 <samuelmwangi729@gmail.com>
2025-08-31 12:48:34 +0300

    second assignmnt

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
@@ -7,9 +6,6 @@ import Footer from "./Footer";
 const App = () => {
   return (
     <div>
-      {/* New app.jsx  */}
-      {/* <WelcomeMessage /> */}
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
       <Footer />
