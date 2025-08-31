d49d143c9926a9b1539d60901d09ad3e26dd1ac6
samuelmwangi729 <samuelmwangi729@gmail.com>
2025-08-31 12:51:52 +0300

    added import user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 const App = () => {
   return (
