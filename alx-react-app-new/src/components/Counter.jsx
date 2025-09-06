2e68504e72fc12e4e112fada38730234702d4b63
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-06 10:58:40 +0300

    renamed the counter

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Counter: React.FC = () => {
+const Counter = () => {
     const [count, setCount] = useState<number>(0)
     return (
         <div style={{display:'flex',justifyContent:'center',alignContent:'center',gap:10}}>
