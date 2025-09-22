ab0f0ee4a01667d71c1dcd5bc8db3707191cfe69
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-22 20:56:00 +0300

    feat: completed the responsive styling on the compnent

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const UserProfile = () => {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://placehold.co/150" className='rounded-full w-36 h-36 mx-auto' alt="User" />
-      <h1 className='text-xl text-blue-800 my-4'>John Doe</h1>
-      <p className='text-gray-600 text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="user-profile bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <img src="https://placehold.co/150" className='rounded-full md:w-36 md:h-36 w-24 h-24 mx-auto' alt="User" />
+      <h1 className='text-lg md:text-xl text-blue-800 my-4'>John Doe</h1>
+      <p className='text-gray-600 text-sm md:text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   )
 }
