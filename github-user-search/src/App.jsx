8d80072aa5a7469bd56d14d041577e850c59dacc
samuel mwangi <samuelmwangi729@gmail.com>
2025-09-22 06:36:18 +0300

    feat:  created the services route

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes,Route } from "react-router-dom";
 import Homepage from "./Homepage";
+import Services from "./services/Services";
 const App = () => {
   return (
     <Routes>
       <Route index element={<Homepage/>}/>
+      <Route path="/services" element={<Services/>}/>
     </Routes>
   )
 }
---
+++ github-user-search/src/services/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      Services here
+    </div>
+  )
+}
+
+export default Services
